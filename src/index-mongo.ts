ba20c4df9491f03fc6c5357dc22367defd48b93a
XaulimMatadordePorco244 <dmichaeldouglas00@gmail.com>
2025-09-17 11:32:04 -0400

    criação de um post de produtos

--- src/index-mongo.ts
+++ src/index-mongo.ts
@@ -14,7 +14,17 @@ app.use(express.json())
 //Criando uma rota para acesso pelo navegador
 app.get('/produtos', async(req:Request, res:Response) => {
     const produtos = await db.collection('produtos').find().toArray()
-    res.json(produtos)
+    res.status(200).json(produtos)
+})
+
+app.post('/produtos', async(req:Request, res:Response) => {
+    const { nome, preco , urlfoto, descricao } = req.body
+    if(!nome || !preco || !urlfoto || !descricao) {
+        return res.status(400).json({ error: 'Dados incompletos' })
+    }
+    const produto = { nome, preco, urlfoto, descricao }
+    const resultado = await db.collection('produtos').insertOne(produto)
+    res.status(201).json({ nome, preco, urlfoto, descricao, _id: resultado.insertedId })
 })
 // Criando o servidor na porta 8000 com express
 app.listen(8000, () => {
